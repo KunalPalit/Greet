6a0fc3a9fef5e4fa1ee46145e0b65dcc8b51d6f5
KunalPalit <kunalpalit006@gmail.com>
2025-03-19 00:22:17 +0530

    Added Alert to meetind > id > page

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -4,17 +4,27 @@ import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
 import { useState } from "react";
 import { useGetCallById } from "@/hooks/useGetCallById";
+import { useParams } from "next/navigation";
 
 import MeetingRoom from "@/components/MeetingRoom";
 import MeetingSetup from "@/components/MeetingSetup";
 import Loader from "@/components/Loader";
+import Alert from "@/components/Alert";
 
-export default function Meeting({ params }: { params: { id: string } }) {
+export default function Meeting() {
+  const { id } = useParams();
   const { user, isLoaded } = useUser();
   const [isSetupComplete, setIsSetupComplete] = useState(false);
-  const { call, isCallLoading } = useGetCallById(params.id);
+  const { call, isCallLoading } = useGetCallById(id);
 
   if (!isLoaded || isCallLoading) return <Loader />;
+
+  const notAllowed =
+    call?.type === "invited" &&
+    (!user || !call.state.members.find((m) => m.user.id === user.id));
+
+  if (notAllowed)
+    return <Alert title="You are not allowed to join this meeting" />;
 
   return (
     <main className="h-screen w-full">
